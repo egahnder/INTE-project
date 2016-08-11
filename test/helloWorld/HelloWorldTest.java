13683487138ea2f9b1d518e45b274f2b3125182b
loxtank <lars.daniel.andersson@gmail.com>
2016-08-11 15:18:17 +0200

    Added classpath and first test

---
+++ test/helloWorld/HelloWorldTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+
+
+
+public class HelloWorldTest {
+    @Test
+    public void testHelloWorld() {
+        HelloWorld greeting = new HelloWorld();
+        assertThat("Hello World", is(equalTo(greeting.getHello())));
+    }
+}
