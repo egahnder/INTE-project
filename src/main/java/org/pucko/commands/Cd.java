076bf2e1f799efce903b9b7f82c970d8d93d26bb
Daniel Andersson <lars.daniel.andersson@gmail.com>
2016-09-05 20:28:31 +0200

    ~ tilde test passing

--- src/main/java/org/pucko/commands/Cd.java
+++ src/main/java/org/pucko/commands/Cd.java
@@ -9,29 +9,32 @@ import org.pucko.core.WorkingDirectory;
 public class Cd {
     private ArrayList<String> args;
     private WorkingDirectory wd;
-    
+
 
     public Cd(ArrayList<String> args, WorkingDirectory wd) {
         this.args = args;
         this.wd = wd;
     }
-    
+
     public boolean execute() {
         Path newPath;
-        
+
         // Lets get the old Path
         Path oldPath = wd.getPath();
-        
+
         // If the argument is ".." create the new Path by calling getParent() on the old path
         // Otherwise create the newPath by sticking the new Path from the args ArrayList onto the oldPath with resolve
-        
-        
+
+
         if (args.get(0).equals("..")) {
             newPath = wd.getPath().getParent();
-        } else {
+        } else if (args.get(0).equals("~")) {
+            String homePath = System.getProperty("user.home");
+            newPath = Paths.get(homePath);
+        } else  {
             newPath = oldPath.resolve(Paths.get(args.get(0)));
         }
-        
+
         // Then we change the path using WorkingDirectorys changePath().
         wd.changePath(newPath);
         return true;
