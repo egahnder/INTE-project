feb4538621f12634ee55b11d2a650de96b693277
Daniel Andersson <lars.daniel.andersson@gmail.com>
2016-09-05 13:59:48 +0200

    Added first test

--- src/test/java/org/pucko/core/WorkingDirectoryTest.java
+++ src/test/java/org/pucko/core/WorkingDirectoryTest.java
@@ -1,5 +1,15 @@
 package org.pucko.core;
 
+import org.junit.Test;
+
 public class WorkingDirectoryTest {
+	
+	
+	@Test
+	public void testWD() {
+		WorkingDirectory wd = new WorkingDirectory();
+	}
+
+
 
 }
