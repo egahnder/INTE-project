a94673c110fdd870b27b6909b288cc0566323371
Eric Egan <egan1551@gmail.com>
2016-09-05 20:52:15 +0200

    Set up structure for CommandRunner

--- src/main/java/org/pucko/core/CommandRunner.java
+++ src/main/java/org/pucko/core/CommandRunner.java
@@ -1,5 +1,14 @@
 package org.pucko.core;
 
+import java.util.ArrayList;
+
+import org.pucko.commands.Command;
+
 public class CommandRunner {
+	
+	public String runCommands(ArrayList<Command> commands){
+		return null;
+		
+	}
 
 }
