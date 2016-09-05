ef22c259977c935fe4ce73be3328e9b40223a8b4
Daniel Andersson <lars.daniel.andersson@gmail.com>
2016-09-05 13:46:11 +0200

    Added skeleton abstract Command

--- src/main/java/org/pucko/commands/Command.java
+++ src/main/java/org/pucko/commands/Command.java
@@ -1,5 +1,42 @@
 package org.pucko.commands;
 
+import java.util.ArrayList;
+
+import org.pucko.core.WorkingDirectory;
+
 public abstract class Command {
 
+	private String name;
+	private String output;
+	private Command pipe;
+	private ArrayList<String> args;
+	private ArrayList<String> validationErrors;
+	private WorkingDirectory workingDirectory;
+	boolean valid;
+
+	public Command(ArrayList<String> commands, WorkingDirectory workingDirectory) {
+
+	}
+
+	public Command(ArrayList<String> commands, WorkingDirectory workingDirectory, Command command) {
+
+	}
+
+	public abstract boolean execute();
+	
+	public abstract void undo();
+	
+	public abstract void addArg(String arg);
+	
+	public abstract String getOutput();
+	
+	public abstract boolean valiadate();
+	
+	public abstract ArrayList<String> getErrors();
+	
+	public abstract void pipeTo(Command command);
+	
+	
+
 }
+
