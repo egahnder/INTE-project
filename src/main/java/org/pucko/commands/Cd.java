8703ac3e53df4e398d150e34d04dc08cd886507e
Daniel Andersson <lars.daniel.andersson@gmail.com>
2016-09-05 20:00:40 +0200

    parentDir test passing

--- src/main/java/org/pucko/commands/Cd.java
+++ src/main/java/org/pucko/commands/Cd.java
@@ -9,6 +9,7 @@ import org.pucko.core.WorkingDirectory;
 public class Cd {
     private ArrayList<String> args;
     private WorkingDirectory wd;
+    
 
     public Cd(ArrayList<String> args, WorkingDirectory wd) {
         this.args = args;
@@ -16,12 +17,20 @@ public class Cd {
     }
     
     public boolean execute() {
+        Path newPath;
         
         // Lets get the old Path
         Path oldPath = wd.getPath();
         
-        //Them create the newPath by sticking the new Path from the args ArrayList onto the oldPath with resolve
-        Path newPath = oldPath.resolve(Paths.get(args.get(0)));
+        // If the argument is ".." create the new Path by calling getParent() on the old path
+        // Otherwise create the newPath by sticking the new Path from the args ArrayList onto the oldPath with resolve
+        
+        
+        if (args.get(0).equals("..")) {
+            newPath = wd.getPath().getParent();
+        } else {
+            newPath = oldPath.resolve(Paths.get(args.get(0)));
+        }
         
         // Then we change the path using WorkingDirectorys changePath().
         wd.changePath(newPath);
