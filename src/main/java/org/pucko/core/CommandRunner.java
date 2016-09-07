4cec97d5f0503cf8f585e2398cc0784b3ae85baf
Eric Egan <egan1551@gmail.com>
2016-09-07 10:18:15 +0200

    Developed basic functionality for CommandRUnner

--- src/main/java/org/pucko/core/CommandRunner.java
+++ src/main/java/org/pucko/core/CommandRunner.java
@@ -1,14 +1,48 @@
 package org.pucko.core;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.pucko.commands.Command;
 
 public class CommandRunner {
+	StringBuilder stringBuilder;
+	public CommandRunner() {
+		stringBuilder = new StringBuilder();
+	}
 	
 	public String runCommands(ArrayList<Command> commands){
-		return null;
-		
+		for (Command command : commands) {
+			if (command.valiadate()) {
+				command.execute();
+				extractOutput(command);
+			}
+			else{
+				extractOutput(command);
+				undoCommands(commands, command);
+				break;
+			}
+		}		
+		return stringBuilder.toString();
+	}
+
+	private void extractOutput(Command command) {
+		String output = command.getOutput();
+		if (output != null && !output.equals("")) {
+			stringBuilder.append(output);			
+			stringBuilder.append(System.lineSeparator());
+		}
+	}
+
+	private void undoCommands(ArrayList<Command> commands, Command command) {
+		int commandIndex = commands.indexOf(command);
+		List<Command> undoList = commands.subList(0, commandIndex);
+		Collections.reverse(undoList);
+		for (Command com : undoList) {
+			extractOutput(com);
+			com.undo();
+		}
 	}
 
 }
--- src/test/java/org/pucko/core/CommandRunnerTest.java
+++ src/test/java/org/pucko/core/CommandRunnerTest.java
@@ -1,5 +1,111 @@
 package org.pucko.core;
 
-public class CommandRunnerTest {
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
 
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.pucko.commands.Command;
+
+public class CommandRunnerTest {
+	ArrayList<Command> commands;
+	Command firstCommand;
+	Command secondCommand;
+	Command thirdCommand;
+	CommandRunner commandRunner;
+	
+	@Before
+	public void setUp(){
+		firstCommand = mock(Command.class);
+		secondCommand = mock(Command.class);
+		thirdCommand = mock(Command.class);
+		commands = new ArrayList<>();
+		commands.add(firstCommand);
+		commands.add(secondCommand);
+		commands.add(thirdCommand);
+		commandRunner = new CommandRunner();
+	}
+	
+	@Test
+	public void testRunnerCallsTestOnAllValidCommands(){
+		setCommandValidations(true, true, true);
+		commandRunner.runCommands(commands);
+		for (Command command : commands) {
+			verify(command, times(1)).execute();
+		}
+	}
+
+	@Test
+	public void testUnvalidCommandIsNotRun(){
+		setCommandValidations(false, true, true);
+		commandRunner.runCommands(commands);
+		verify(firstCommand, times(0)).execute();
+	}
+	
+	@Test
+	public void testRemainingCommandsAreNotCalledAfterInvalidCommand(){
+		setCommandValidations(false, true, true);
+		commandRunner.runCommands(commands);
+		verify(secondCommand, times(0)).execute();
+		verify(thirdCommand, times(0)).execute();
+	}
+	
+	@Test
+	public void testInvalidCommandWillCallUndoOnPreviousCommands(){
+		InOrder inOrder = inOrder(firstCommand, secondCommand);
+		setCommandValidations(true, true, false);
+		commandRunner.runCommands(commands);
+		inOrder.verify(secondCommand, times(1)).undo();
+		inOrder.verify(firstCommand, times(1)).undo();
+		
+	}
+	
+	@Test
+	public void testValidCommandsReturnOutput(){
+		setCommandValidations(true, true, true);
+		setCommandOutput("test1", "test2", "test3");
+		String output = commandRunner.runCommands(commands);
+		assertEquals("test1\ntest2\ntest3\n", output);
+	}
+	
+	@Test
+	public void testUnvalidCommandReturnsOutput(){
+		setCommandValidations(true, true, false);
+		setCommandOutput("test1", "test2", "test3");
+		String output = commandRunner.runCommands(commands);
+		assertEquals("test1\ntest2\ntest3\ntest2\ntest1\n", output);	
+	}
+	
+	@Test
+	public void testNullStringOutputIsNotShown(){
+		setCommandOutput("test1", null, "test3");
+		setCommandValidations(true, true, true);
+		String output = commandRunner.runCommands(commands);
+		assertEquals("test1\ntest3\n", output);
+	}
+	@Test
+	public void testEmptyStringOutputIsNotShown(){
+		setCommandOutput("test1", "", "test3");
+		setCommandValidations(true, true, true);
+		String output = commandRunner.runCommands(commands);
+		assertEquals("test1\ntest3\n", output);
+	}
+	
+	private void setCommandValidations(boolean first, boolean second, boolean third){
+		when(firstCommand.valiadate()).thenReturn(first);
+		when(secondCommand.valiadate()).thenReturn(second);
+		when(thirdCommand.valiadate()).thenReturn(third);
+	}
+	
+	private void setCommandOutput(String first, String second, String third){
+		when(firstCommand.getOutput()).thenReturn(first);
+		when(secondCommand.getOutput()).thenReturn(second);
+		when(thirdCommand.getOutput()).thenReturn(third);
+	}
+	
+	
 }
