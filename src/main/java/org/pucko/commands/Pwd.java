3f330722852f5ee676b013e431349ef1505c530e
Tobias Ahnhem <tobias.ahnhem@gmail.com>
2016-09-08 21:10:53 +0200

    Pwd/PwdTest finished

--- src/main/java/org/pucko/commands/Pwd.java
+++ src/main/java/org/pucko/commands/Pwd.java
@@ -2,19 +2,20 @@ package org.pucko.commands;
 
 import java.util.ArrayList;
 
+import org.pucko.core.OutputHandler;
 import org.pucko.core.WorkingDirectory;
 
 public class Pwd extends Command {
 
-	public Pwd(ArrayList<String> args, WorkingDirectory wd) {
-		super(args, wd);
+	public Pwd(ArrayList<String> args, WorkingDirectory wd, OutputHandler oh) {
+		super(args, wd, oh);
 	}
 
 	@Override
 	public boolean execute() {
 
 		if (validate()) {
-			output = workingDirectory.getPath().toString();
+			output(workingDirectory.getPath().toString());
 			return true;
 		} else {
 			return false;
--- src/test/java/org/pucko/commands/PwdTest.java
+++ src/test/java/org/pucko/commands/PwdTest.java
@@ -12,11 +12,13 @@ import static org.mockito.Mockito.*;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
+import org.pucko.core.OutputHandler;
 import org.pucko.core.WorkingDirectory;
 
 public class PwdTest {
 
 	private WorkingDirectory wd;
+	private OutputHandler oh;
 	private ArrayList<String> input;
 
 	@Rule
@@ -25,31 +27,30 @@ public class PwdTest {
 	@Before
 	public void setUp() {
 		wd = mock(WorkingDirectory.class);
+		oh = mock(OutputHandler.class);
 		input = new ArrayList<>();
 	}
-
-	// Pwd ska bara köra om args är 0 eller null. args.size() > 0 blir false i
-	// validate()
 
 	@Test
 	public void testExecuteSetsOutput() {
 		Path tempDir = testFolder.getRoot().toPath();
 		when(wd.getPath()).thenReturn(tempDir);
-		Pwd p = new Pwd(input, wd);
+		Pwd p = new Pwd(input, wd, oh);
 		p.execute();
-		assertEquals(tempDir.toAbsolutePath().toString(), p.getOutput());
+		verify(oh, times(1)).handleOutput(tempDir.toAbsolutePath().toString());
+
 	}
 
 	@Test
 	public void testValidateReturnTrue() {
-		Pwd p = new Pwd(input, wd);
+		Pwd p = new Pwd(input, wd, oh);
 		assertTrue(p.validate());
 
 	}
 
 	@Test
 	public void testValidateReturnFalse() {
-		Pwd p = new Pwd(input, null);
+		Pwd p = new Pwd(input, null, oh);
 		assertFalse(p.validate());
 
 	}
