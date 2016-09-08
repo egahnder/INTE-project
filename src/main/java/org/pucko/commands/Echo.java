73864804be2319c37b53abdb9fe02f42ead4004f
Tobias Ahnhem <tobias.ahnhem@gmail.com>
2016-09-08 11:15:38 +0200

    Started on echo

--- src/main/java/org/pucko/commands/Echo.java
+++ src/main/java/org/pucko/commands/Echo.java
@@ -1,5 +1,31 @@
 package org.pucko.commands;
 
-public class Echo {
+import java.util.ArrayList;
+
+import org.pucko.core.WorkingDirectory;
+
+public class Echo extends Command {
+
+	public Echo(ArrayList<String> args, WorkingDirectory wd) {
+		super(args, wd);
+	}
+
+	@Override
+	public boolean execute() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void undo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean validate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- src/test/java/org/pucko/commands/EchoTest.java
+++ src/test/java/org/pucko/commands/EchoTest.java
@@ -1,5 +1,45 @@
 package org.pucko.commands;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.pucko.core.WorkingDirectory;
+
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+
+
 public class EchoTest {
 
+	private WorkingDirectory wd;
+	private ArrayList<String> validArgs;
+	private ArrayList<String> invalidArgs;
+	
+	
+	
+	@Before
+	public void setUp(){
+		wd = mock(WorkingDirectory.class);
+		validArgs = new ArrayList<>();
+		validArgs.add("Hello World");
+		invalidArgs = new ArrayList<>();
+		invalidArgs.add(null);
+		invalidArgs.add("");
+	}
+	
+	
+	@Test
+	public void testExecute(){
+		
+		Echo e = new Echo(validArgs, wd);
+		assertTrue(e.execute());
+		
+	}
+	
+	
+	
+	
+	
 }
