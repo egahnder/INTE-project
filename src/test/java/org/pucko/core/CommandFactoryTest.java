f00be9b94912d9c312c4e8cb4725b15d61797da3
Daniel Andersson <lars.daniel.andersson@gmail.com>
2016-09-10 21:25:26 +0200

    initial CommandFactory tests

--- src/test/java/org/pucko/core/CommandFactoryTest.java
+++ src/test/java/org/pucko/core/CommandFactoryTest.java
@@ -1,5 +1,55 @@
 package org.pucko.core;
 
-public class CommandFactoryTest {
 
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.pucko.commands.Command;
+import org.junit.Before;
+import org.junit.Rule;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
+public class CommandFactoryTest {
+    
+    private CommandFactory cf;
+    private WorkingDirectory wd;
+    private OutputHandler oh;
+    private String command;
+    ArrayList<Command> returnedCommands;
+    
+    @Before
+    public void setUp() throws Exception {
+        
+        
+        cf = new CommandFactory();
+        wd = new WorkingDirectory(Paths.get("/tmp"));
+        oh = mock(OutputHandler.class);
+        command = "cd ..";
+        
+    }
+    
+    @Test
+    public void testCFReturnsArrayList() {
+        returnedCommands = cf.createCommands(command, wd, oh);
+        
+        assertTrue(returnedCommands instanceof ArrayList<?>);
+        
+    }
+    
+    @Test
+    public void testCFReturnsNonEmptyArrayList() {
+         
+        returnedCommands = cf.createCommands(command, wd, oh);
+        
+        
+        assertFalse(returnedCommands.isEmpty());
+        
+    }
 }
