fdb743dab92fb1e8bf64a615158493c932c57c2b
Daniel Andersson <lars.daniel.andersson@gmail.com>
2016-09-10 21:25:40 +0200

    Inital tests passing

--- src/main/java/org/pucko/core/CommandFactory.java
+++ src/main/java/org/pucko/core/CommandFactory.java
@@ -2,12 +2,23 @@ package org.pucko.core;
 
 import java.util.ArrayList;
 
+import org.pucko.commands.Cd;
 import org.pucko.commands.Command;
 
+
 public class CommandFactory {
-	
+    ArrayList<Command> createdCommand = new ArrayList<>();
+    ArrayList<String> args = new ArrayList<>();
+    
 	public ArrayList<Command> createCommands(String commandString, WorkingDirectory workingDirectory, OutputHandler outputHandler){
-		return null;		
+	    	   
+	    args.add("..");
+	    
+	    Cd cd = new Cd(args, workingDirectory);
+	    
+	    createdCommand.add(cd);
+	    
+		return createdCommand;		
 	}
 
 }
