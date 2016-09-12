5e64a4112ca2d81be8dcbb4a5063531407a680c6
loxtank <lars.daniel.andersson@gmail.com>
2016-09-12 21:09:50 +0200

    Fixed varius small issues, implemented defaultCommand

--- src/main/java/org/pucko/core/CommandFactory.java
+++ src/main/java/org/pucko/core/CommandFactory.java
@@ -1,41 +1,37 @@
 package org.pucko.core;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
-import org.pucko.commands.CdCommandMethod;
-import org.pucko.commands.Command;
-import org.pucko.commands.CommandMethod;
-import org.pucko.commands.EchoCommandMethod;
+import org.pucko.commands.*;
 
 public class CommandFactory {
-    ArrayList<Command> createdCommand = new ArrayList<>();
-    ArrayList<String> args = new ArrayList<>();
-    HashMap<String, CommandMethod> validCommands = new HashMap<>();
-    String[] allArgs;
-    WorkingDirectory wd;
-    
-    
+    private HashMap<String, CommandMethod> validCommands = new HashMap<>();
+
     public CommandFactory() {
         validCommands.put("cd", new CdCommandMethod());
         validCommands.put("echo", new EchoCommandMethod());
     }
     
 	public ArrayList<Command> createCommands(String commandString, WorkingDirectory workingDirectory, OutputHandler outputHandler){
-	    this.wd = workingDirectory;
-	    allArgs = commandString.split("[\\s\\xA0]+");
-	    
+		String[] allArgs = commandString.split("[\\s\\xA0]+");
+		ArrayList<Command> createdCommand = new ArrayList<>();
+
 	    if (!validCommands.containsKey(allArgs[0])) {
-	        System.out.println("Invalid Command");
-	        return null;
+	        ArrayList<String> invalidCommand = new ArrayList<>();
+            invalidCommand.add("Invalid Command");
+            createdCommand.add(new DefaultCommand(invalidCommand, workingDirectory, outputHandler));
+            return createdCommand;
+
 	    }
 	    
 	   String commandToCreate = allArgs[0];
-	   
+
 	   // Remove the command from allArgs array
 	   allArgs = Arrays.copyOfRange(allArgs, 1, allArgs.length);
-	   createdCommand.add(validCommands.get(commandToCreate).runMethod(allArgs, wd, outputHandler));
+	   createdCommand.add(validCommands.get(commandToCreate).runMethod(allArgs, workingDirectory, outputHandler));
 	    
 		return createdCommand;		
 	}
