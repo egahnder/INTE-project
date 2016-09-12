37c7d5a52af561a49c35837457dd6d5d33c80797
Eric Egan <egan1551@gmail.com>
2016-09-12 13:16:59 +0200

    Program now runs menu

--- src/main/java/org/pucko/core/Program.java
+++ src/main/java/org/pucko/core/Program.java
@@ -1,11 +1,16 @@
 package org.pucko.core;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-
-//hej
-		
+		CommandFactory commandFactory = new CommandFactory();
+		CommandRunner commandRunner = new CommandRunner();
+		WorkingDirectory workingDirectory = new WorkingDirectory();
+		Controller controller = new Controller(workingDirectory, commandRunner, commandFactory);
+		Menu menu = new Menu(controller, new Scanner(System.in));
+		menu.run();
 	}
 
 }
