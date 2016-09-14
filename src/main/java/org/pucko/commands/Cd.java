a7ed2b90959e12b304dcfa384187197194c67834
Daniel Andersson <lars.daniel.andersson@gmail.com>
2016-09-14 14:06:09 +0200

    Reactored cd to use errorhandler, printserrorOnNoParam passing

--- src/main/java/org/pucko/commands/Cd.java
+++ src/main/java/org/pucko/commands/Cd.java
@@ -11,37 +11,41 @@ import org.pucko.core.WorkingDirectory;
 public class Cd extends Command {
     private Path newPath;
     private Path oldPath = getWorkingDirectory();
-    
 
 
-    public Cd(ArrayList<String> args, WorkingDirectory wd, OutputHandler oh) {
-        super(args, wd, oh);
-      
+    public Cd(ArrayList<String> args, WorkingDirectory workingDirectory, OutputHandler outputHandler) {
+        super(args, workingDirectory, outputHandler);
+
+    }
+
+    public Cd(ArrayList<String> args, WorkingDirectory workingDirectory, OutputHandler outputHandler, OutputHandler errorHandler) {
+        super(args, workingDirectory, outputHandler, errorHandler);
     }
 
     public boolean execute() {
-        
-        // Then we change the path using WorkingDirectorys changePath().
+
         setWorkingDirectory(newPath);
+
         return true;
     }
-    
+
     private void resolveNewPath() {
-        
+
         // If the argument is ".." create the new Path by calling getParent() on the old path
         // If the argument is "~" create a Path to user home
         // Otherwise create the newPath by sticking the new Path from the args ArrayList onto the oldPath with resolve
-            if (getArg(0).equals("..")) {
-                newPath = oldPath.getParent();
-            } else if (getArg(0).equals("~")) {
-                String homePath = System.getProperty("user.home");
-                newPath = Paths.get(homePath);
-            } else if (getArg(0).equals("/")) {
-                newPath = Paths.get("/");
-            } else {
-                newPath = oldPath.resolve(Paths.get(getArg(0)));
 
-            }
+        if (getArg(0).equals("..")) {
+            newPath = oldPath.getParent();
+        } else if (getArg(0).equals("~")) {
+            String homePath = System.getProperty("user.home");
+            newPath = Paths.get(homePath);
+        } else if (getArg(0).equals("/")) {
+            newPath = Paths.get("/");
+        } else {
+            newPath = oldPath.resolve(Paths.get(getArg(0)));
+
+        }
     }
 
     @Override
@@ -52,29 +56,37 @@ public class Cd extends Command {
     @Override
     protected boolean verifyExecutable() {
 
-        // If args contains null, return false
-        if (getArg(0) == null) {
+        // If args is null, return false
+        try {
+            getArg(0);
+        } catch (IndexOutOfBoundsException | NullPointerException e) {
+            error("ERROR: No argument provided");
             return false;
         }
-        
-        // Lets resolve the new Path and set newPath
+
+        if (getArg(0) == null) {
+            error("ERROR: No argument provided");
+            return false;
+        }
 
         resolveNewPath();
 
         // Lets make sure the path exists
-        if (newPath==null) {
+        if (newPath == null) {
             return false;
+
         }
+
         //Lets make sure the directory exists
         if (!Files.exists(newPath)) {
             return false;
         }
-        
-      //Lets make sure the directory is readable
+
+        //Lets make sure the directory is readable
         if (!Files.isReadable(newPath)) {
             return false;
         }
-       
+
         // If all tests pass, this command is validated
         return true;
     }
