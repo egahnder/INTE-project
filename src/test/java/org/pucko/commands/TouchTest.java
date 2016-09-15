bfcd40087b95995b6d718307e0949080a03f8b57
Tobias Ahnhem <tobias.ahnhem@gmail.com>
2016-09-15 12:18:34 +0200

    Refectored TouchTest

--- src/test/java/org/pucko/commands/TouchTest.java
+++ src/test/java/org/pucko/commands/TouchTest.java
@@ -1,32 +1,34 @@
 
 package org.pucko.commands;
 
-        /**
-         * Created by Tobias on 15/09/16.
-         */
+/**
+ * Created by Tobias on 15/09/16.
+ */
 
-        import static org.junit.Assert.*;
-        import org.junit.Test;
-        import org.junit.rules.TemporaryFolder;
+import static org.junit.Assert.*;
 
-        import static org.mockito.Mockito.*;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-        import java.io.File;
-        import java.nio.file.Files;
-        import java.io.IOException;
-        import java.nio.file.Path;
-        import java.util.ArrayList;
+import static org.mockito.Mockito.*;
 
-        import org.pucko.core.OutputHandler;
-        import org.pucko.core.WorkingDirectory;
-        import org.pucko.testutilities.ArgsPopulator;
-        import org.junit.Before;
-        import org.junit.Rule;
+import java.io.File;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import org.pucko.core.OutputHandler;
+import org.pucko.core.WorkingDirectory;
+import org.pucko.testutilities.ArgsPopulator;
+import org.junit.Before;
+import org.junit.Rule;
 
 public class TouchTest {
 
     private WorkingDirectory wd;
     private OutputHandler oh;
+    private OutputHandler eh;
     private ArgsPopulator ap;
 
     @Rule
@@ -36,18 +38,21 @@ public class TouchTest {
     public void setUp() {
         wd = mock(WorkingDirectory.class);
         oh = mock(OutputHandler.class);
+        eh = mock(OutputHandler.class);
         ap = new ArgsPopulator();
     }
 
+    
     @Test
     public void testExecuteCreatesSingleFile() throws IOException {
 
         Path folderPath = testFolder.getRoot().toPath();
         when(wd.getPath()).thenReturn(folderPath);
 
-        String[] input = { "filnamn" };
+        String[] input = {"filnamn"};
 
-        Touch t = new Touch(ap.populate(input), wd, oh);
+
+        Touch t = new Touch(ap.populate(input), wd, oh, eh);
 
         Path filePath = folderPath.resolve("filnamn");
         t.execute();
