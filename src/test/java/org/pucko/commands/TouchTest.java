ea6a6cf775266e52fb64f2d583575f2f75a48702
Tobias Ahnhem <tobias.ahnhem@gmail.com>
2016-09-15 10:55:05 +0200

    testExecute finished

---
+++ src/test/java/org/pucko/commands/TouchTest.java
@@ -0,0 +1,63 @@
+
+package org.pucko.commands;
+
+        /**
+         * Created by Tobias on 15/09/16.
+         */
+
+        import static org.junit.Assert.*;
+        import org.junit.Test;
+        import org.junit.rules.TemporaryFolder;
+
+        import static org.mockito.Mockito.*;
+
+        import java.io.File;
+        import java.nio.file.Files;
+        import java.io.IOException;
+        import java.nio.file.Path;
+        import java.util.ArrayList;
+
+        import org.pucko.core.OutputHandler;
+        import org.pucko.core.WorkingDirectory;
+        import org.pucko.testutilities.ArgsPopulator;
+        import org.junit.Before;
+        import org.junit.Rule;
+
+public class TouchTest {
+
+    private WorkingDirectory wd;
+    private OutputHandler oh;
+    private ArgsPopulator ap;
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
+    @Before
+    public void setUp() {
+        wd = mock(WorkingDirectory.class);
+        oh = mock(OutputHandler.class);
+        ap = new ArgsPopulator();
+    }
+
+    @Test
+    public void testExecuteCreatesSingleFile() throws IOException {
+
+        Path folderPath = testFolder.getRoot().toPath();
+        when(wd.getPath()).thenReturn(folderPath);
+
+        String[] input = { "filnamn" };
+
+        Touch t = new Touch(ap.populate(input), wd, oh);
+
+        Path filePath = folderPath.resolve("filnamn");
+        t.execute();
+        assertTrue(new File(filePath.toString()).isFile());
+
+    }
+
+    @Test
+    public void testExecuteCreatesFileWithExtension() {
+
+    }
+
+}
