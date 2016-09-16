f8139e4ec55156d4c0ad3847d6dd1b8f133ffa46
Daniel Andersson <lars.daniel.andersson@gmail.com>
2016-09-16 10:24:04 +0200

    CommandParser no longer removes Command string from argument List

--- src/main/java/org/pucko/core/CommandParser.java
+++ src/main/java/org/pucko/core/CommandParser.java
@@ -14,17 +14,16 @@ public class CommandParser {
 
     public ArrayList<Command> parseCommands(String commands, WorkingDirectory workingDirectory, OutputHandler outputHandler){
         ArrayList<Command> commandList = new ArrayList<>();
+
         String[] commandArray = commands.split("\\s+");
         String commandString = commandArray[0];
-        if (commandArray.length > 1) {
-            commandArray = Arrays.copyOfRange(commandArray, 1, commandArray.length);
-        }
-        else {
-            commandArray = new String[0];
-        }
+
         ArrayList<String> args = new ArrayList<>(Arrays.asList(commandArray));
+
         Command command = commandFactory.createCommand(commandString, args, workingDirectory, outputHandler);
+
         commandList.add(command);
+
         return commandList;
     }
 }
