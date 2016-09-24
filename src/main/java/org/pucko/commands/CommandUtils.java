7c559e3e8eaaa8ae25b2e6bc270459d6c189236f
Eric Egan <egan1551@gmail.com>
2016-09-24 13:49:10 +0200

    Developed and tested CommandUtils

--- src/main/java/org/pucko/commands/CommandUtils.java
+++ src/main/java/org/pucko/commands/CommandUtils.java
@@ -1,29 +1,93 @@
 package org.pucko.commands;
 
+import com.google.common.collect.ImmutableList;
 import org.pucko.core.InputHandler;
 import org.pucko.core.OutputHandler;
 import org.pucko.core.WorkingDirectory;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 /**
  * Created by eric on 2016-09-24.
  */
 public class CommandUtils {
+    private WorkingDirectory workingDirectory;
+    private OutputHandler outputHandler;
+    private OutputHandler errorHandler;
+    private InputHandler inputHandler;
+    private ArrayList<String> args;
+
+    private CommandUtils(ArrayList<String> args, InputHandler inputHandler, OutputHandler errorHandler, OutputHandler outputHandler, WorkingDirectory workingDirectory) {
+        this.workingDirectory = workingDirectory;
+        this.outputHandler = outputHandler;
+        this.errorHandler = errorHandler;
+        this.inputHandler = inputHandler;
+        this.args = args;
+    }
+
+    public static UtilsBuilder builder(){
+        return new UtilsBuilder();
+    }
+
+    public Path getWorkingDirectory() {
+        return null;
+    }
+
+    public void changeWorkingDirectory(Path path) {
+        workingDirectory.changePath(path);
+    }
+
+    public void output(String output) {
+        outputHandler.handleOutput(output);
+    }
+
+    public void error(String error) {
+        errorHandler.handleOutput(error);
+    }
+
+    public ImmutableList<String> getHistory() {
+        return ImmutableList.copyOf(inputHandler.getHistory());
+    }
+
+    public ImmutableList<String> getArgs() {
+        return ImmutableList.copyOf(args);
+    }
+
+    public void addArg(String arg) {
+        args.add(arg);
+    }
 
     public static class UtilsBuilder{
+        private WorkingDirectory workingDirectory;
+        private OutputHandler outputHandler;
+        private OutputHandler errorHandler;
+        private InputHandler inputHandler;
+        private ArrayList<String> args;
+
         public UtilsBuilder addWorkingDirectory(WorkingDirectory workingDirectory){
-            return null;
+            this.workingDirectory = workingDirectory;
+            return this;
         }
-        public UtilsBuilder addErrorHandler(OutputHandler outputHandler){
-            return null;
+        public UtilsBuilder addErrorHandler(OutputHandler errorHandler){
+            this.errorHandler = errorHandler;
+            return this;
         }
         public UtilsBuilder addOutputHandler(OutputHandler outputHandler){
-            return null;
+            this.outputHandler = outputHandler;
+            return this;
         }
         public UtilsBuilder addInputHandler(InputHandler inputHandler){
-            return null;
+            this.inputHandler = inputHandler;
+            return this;
+        }
+
+        public UtilsBuilder addArgs(ArrayList<String> args){
+            this.args = args;
+            return this;
         }
         public CommandUtils build(){
-            return null;
+            return new CommandUtils(args, inputHandler, errorHandler, outputHandler, workingDirectory);
         }
     }
 }
