4433249df2e1652954d85a3d1074f7bc247b3a33
Tobias Ahnhem <tobias.ahnhem@gmail.com>
2016-10-13 11:11:24 +0200

    Added tests for Pwd to achieve 100% coverage

--- src/test/java/org/pucko/commands/PwdTest.java
+++ src/test/java/org/pucko/commands/PwdTest.java
@@ -5,35 +5,52 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import java.nio.file.Path;
-import java.util.ArrayList;
 
 import org.mockito.Mock;
 
 public class PwdTest {
 
-	@Mock
+    @Mock
     private CommandUtils commandUtils;
 
-	@Rule
-	public TemporaryFolder testFolder = new TemporaryFolder();
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
 
-	@Before
-	public void setUp() {
+    @Before
+    public void setUp() {
         initMocks(this);
-	}
+    }
 
-	@Test
-	public void testExecuteSetsOutput() {
-		Path tempDir = testFolder.getRoot().toPath();
-		when(commandUtils.getWorkingDirectory()).thenReturn(tempDir);
-		Pwd p = new Pwd(commandUtils);
-		p.execute();
-		verify(commandUtils, times(1)).output(tempDir.toAbsolutePath().toString());
+    @Test
+    public void testExecuteSetsOutput() {
+        Path tempDir = testFolder.getRoot().toPath();
+        when(commandUtils.getWorkingDirectory()).thenReturn(tempDir);
+        Pwd p = new Pwd(commandUtils);
+        p.execute();
+        verify(commandUtils, times(1)).output(tempDir.toAbsolutePath().toString());
 
-	}
+    }
+
+    @Test
+    public void testVerifyExecutableReturnsTrue() {
+
+        Path tempDir = testFolder.getRoot().toPath();
+        when(commandUtils.getWorkingDirectory()).thenReturn(tempDir);
+        Pwd p = new Pwd(commandUtils);
+        assertTrue(p.verifyExecutable());
+
+    }
+
+    @Test
+    public void testVerifyExecutableReturnsFalse(){
+        when(commandUtils.getWorkingDirectory()).thenReturn(null);
+        Pwd p = new Pwd(commandUtils);
+        assertFalse(p.verifyExecutable());
+    }
 
 }
