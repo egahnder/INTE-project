111e324ac2f59c46df118482242d5f1ee894ae6a
Tobias Ahnhem <tobias.ahnhem@gmail.com>
2016-10-18 10:10:04 +0200

    Added command test

--- src/test/java/org/pucko/commands/CommandTest.java
+++ src/test/java/org/pucko/commands/CommandTest.java
@@ -1,5 +1,33 @@
 package org.pucko.commands;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+
 public class CommandTest {
+
+
+    @Mock
+    private CommandUtils commandUtils;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+    }
+
+    @Test
+    public void testCommandHandleOutput() {
+        Echo e = new Echo(commandUtils);
+        e.handleOutput("test output");
+        verify(commandUtils, times(1)).addArg("test output");
+
+
+    }
+
 
 }
